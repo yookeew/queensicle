f5d9eac8237d8aefc0fc8e551020f66a22ae21c6
Yuqi Wang <Y.Wang-237@student.tudelft.nl>
2026-02-08 23:00:47 +0100

    ayyy works

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,36 +1,167 @@
-import java.sql.SQLOutput;
-import java.util.ArrayList;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.util.*;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
-        // 0=purple, 1=blue, 2=orange, 3=green, 4=gray, 5=dark blue, 6=red, 7=yellow
-        //answer: (0,5), (1,2), (2,0), (3,3), (4,1), (5,4), (6,6)
-        Puzzle test = createTest();
-        Solver solver = new Solver(test);
+
+    public static void main(String[] args) throws Exception {
+        WebDriverManager.chromedriver().setup();
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter URL: ");
+        String url = scanner.nextLine();
+
+        System.out.print("Enter board size (e.g., 7): ");
+        int gridSize = scanner.nextInt();
+
+        WebDriver driver = new ChromeDriver();
+        driver.get(url);
+        Thread.sleep(2000);
+
+        WebElement board = driver.findElement(By.className("board"));
+
+        int boardX = board.getLocation().getX();
+        int boardY = board.getLocation().getY();
+        int boardWidth = board.getSize().getWidth();
+        int boardHeight = board.getSize().getHeight();
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        double dpr = ((Number) js.executeScript("return window.devicePixelRatio")).doubleValue();
+
+        File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        BufferedImage fullImg = ImageIO.read(screenshot);
+
+        int sx = (int) (boardX * dpr);
+        int sy = (int) (boardY * dpr);
+        int sw = (int) (boardWidth * dpr);
+        int sh = (int) (boardHeight * dpr);
+
+        BufferedImage boardImg = fullImg.getSubimage(sx, sy, sw, sh);
+
+        List<List<Integer>> colorBoard = parseBoard(boardImg, gridSize, 30);
+
+        Solver solver = new Solver(new Puzzle(gridSize, colorBoard));
         List<List<Integer>> solution = solver.solve();
-        System.out.println(solution);
+
+        if (solution == null) {
+            System.out.println("No solution found");
+            driver.quit();
+            return;
+        }
+
+        Actions actions = new Actions(driver);
+
+        for (List<Integer> pos : solution) {
+            int row = pos.get(0);
+            int col = pos.get(1);
+
+            WebElement square = driver.findElement(
+                    By.cssSelector(".square[data-row='" + row + "'][data-col='" + col + "']")
+            );
+
+            actions.moveToElement(square)
+                    .doubleClick()
+                    .perform();
+
+            Thread.sleep(200);
+
+        }
+
+        System.out.println("Done!");
+        Thread.sleep(5000);
+        driver.quit();
     }
-    public static Puzzle createTest() {
-        int[][] regions = {
-                {0, 0, 0, 1, 3, 4, 4},
-                {0, 1, 1, 1, 3, 4, 4},
-                {0, 2, 2, 2, 3, 3, 4},
-                {0, 2, 2, 3, 3, 4, 4},
-                {0, 2, 2, 3, 3, 4, 4},
-                {0, 0, 0, 3, 5, 4, 4},
-                {5, 5, 5, 5, 5, 5, 6}
-        };
+
+    static List<List<Integer>> parseBoard(BufferedImage img, int gridSize, int threshold) {
+        int cellWidth = img.getWidth() / gridSize;
+        int cellHeight = img.getHeight() / gridSize;
+
+        List<List<Color>> colors = new ArrayList<>();
+
+        for (int i = 0; i < gridSize; i++) {
+            List<Color> row = new ArrayList<>();
+            for (int j = 0; j < gridSize; j++) {
+
+                double[] sx = {0.5, 0.3, 0.7, 0.3, 0.7};
+                double[] sy = {0.5, 0.3, 0.3, 0.7, 0.7};
+
+                int rSum = 0, gSum = 0, bSum = 0, count = 0;
+
+                for (int s = 0; s < sx.length; s++) {
+                    int x = (int) (j * cellWidth + cellWidth * sx[s]);
+                    int y = (int) (i * cellHeight + cellHeight * sy[s]);
+
+                    int rgb = img.getRGB(x, y);
+                    int r = (rgb >> 16) & 0xFF;
+                    int g = (rgb >> 8) & 0xFF;
+                    int b = rgb & 0xFF;
+
+                    if (!(r < 10 && g < 10 && b < 10)) {
+                        rSum += r;
+                        gSum += g;
+                        bSum += b;
+                        count++;
+                    }
+                }
+
+                if (count > 0) {
+                    row.add(new Color(rSum / count, gSum / count, bSum / count));
+                } else {
+                    row.add(Color.BLACK);
+                }
+            }
+            colors.add(row);
+        }
+
+        List<Color> clusters = new ArrayList<>();
+
+        for (int i = 0; i < gridSize; i++) {
+            for (int j = 0; j < gridSize; j++) {
+                Color c = colors.get(i).get(j);
+
+                int id = -1;
+                for (int k = 0; k < clusters.size(); k++) {
+                    if (colorDistance(c, clusters.get(k)) < threshold) {
+                        id = k;
+                        break;
+                    }
+                }
+
+                if (id == -1) {
+                    clusters.add(c);
+                    id = clusters.size() - 1;
+                }
+
+                colors.get(i).set(j, new Color(id, id, id));
+            }
+        }
 
         List<List<Integer>> board = new ArrayList<>();
-        for (int i = 0; i < 7; i++) {
+        for (int i = 0; i < gridSize; i++) {
             List<Integer> row = new ArrayList<>();
-            for (int j = 0; j < 7; j++) {
-                row.add(regions[i][j]);
+            for (int j = 0; j < gridSize; j++) {
+                row.add(colors.get(i).get(j).getRed());
             }
             board.add(row);
         }
 
-        return new Puzzle(7, board);
+        System.out.println("Detected " + clusters.size() + " unique colors");
+        return board;
+    }
+
+    static double colorDistance(Color a, Color b) {
+        return Math.sqrt(
+                Math.pow(a.getRed() - b.getRed(), 2) +
+                        Math.pow(a.getGreen() - b.getGreen(), 2) +
+                        Math.pow(a.getBlue() - b.getBlue(), 2)
+        );
     }
 }
